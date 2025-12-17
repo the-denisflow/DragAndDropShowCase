6fb7c5c33bda8d1056aa8be101ea15b69829c8fa
the-denisflow <anis24allger@gmail.com>
2025-12-17 21:55:58 +0100

    feat: start implement draggble item

--- app/src/main/java/com/example/myapplication/presentation/components/mainpage/SquaredDraggableItem.kt
+++ app/src/main/java/com/example/myapplication/presentation/components/mainpage/SquaredDraggableItem.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.presentation.components.mainpage
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
@@ -9,14 +10,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.layer.GraphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.domain.model.TileStateData
+import com.example.myapplication.presentation.components.dragndrop.DragAndDropState
 
 @Composable
 fun SquaredDraggableItem (
     modifier: Modifier,
-    element : TileStateData
+    element : TileStateData,
+    itemGraphicsLayer: GraphicsLayer,
+    dragAndDropState: DragAndDropState,
+    index: Int
 ) {
     Box(
         modifier = Modifier
@@ -25,7 +32,17 @@ fun SquaredDraggableItem (
             .border(
                 width = 2.dp,
                 color = Color.Black
-            ),
+            ).pointerInput(Unit) {
+                detectDragGesturesAfterLongPress(
+                    onDragStart = { offset ->
+
+                    },
+                    onDrag = { change, _ -> change.consume() },
+                    onDragEnd = { },
+                    onDragCancel = { }
+                )
+            }
+        ,
             contentAlignment = Alignment.Center
 
     ) {
